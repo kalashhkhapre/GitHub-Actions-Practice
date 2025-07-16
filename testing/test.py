13ba8ec21c06f344b945358ac725f82e5724df50
Kalash Khapre <147745180+kalashhkhapre@users.noreply.github.com>
2025-07-17 02:37:58 +0530

    initial commit

---
+++ testing/test.py
@@ -0,0 +1,20 @@
+# tests/test_calculator.py
+
+import pytest
+from src.python_app import add, subtract, multiply, divide
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+
+def test_multiply():
+    assert multiply(3, 4) == 12
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(5, 0)
